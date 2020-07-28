ba281634acab1f07dee9c73da133be22508038c0
LoveDesignUA <lovedesignua@gmail.com>
2020-07-29 01:41:20 +0300

    something formspree

--- src/components/button/ripple-button.js
+++ src/components/button/ripple-button.js
@@ -11,6 +11,7 @@ const RippleButton = ({
   secondary,
   fullWidth,
   onClickHandler,
+  ...rest
 }) => {
   const [coords, setCoords] = React.useState({ x: -1, y: -1 })
   const [isRippling, setIsRippling] = React.useState(false)
@@ -41,17 +42,16 @@ const RippleButton = ({
     }
   )
 
+  const onButtonClick = e => {
+    var rect = e.target.getBoundingClientRect()
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+    setCoords({ x, y })
+    onClickHandler && onClickHandler(e)
+  }
+
   return (
-    <button
-      className={buttonStyles}
-      onClick={e => {
-        var rect = e.target.getBoundingClientRect()
-        var x = e.clientX - rect.left
-        var y = e.clientY - rect.top
-        setCoords({ x, y })
-        onClickHandler && onClickHandler(e)
-      }}
-    >
+    <button className={buttonStyles} onClick={onButtonClick} {...rest}>
       {isRippling ? (
         <span
           className="ripple"
